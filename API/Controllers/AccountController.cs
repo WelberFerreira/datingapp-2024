76d3b567e3b291086a418ef0c7de38db00398752
WelberFerreira <welber.fa@gmail.com>
2025-11-17 18:24:30 -0300

    End of Section 5

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -17,26 +17,27 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
 {
     [HttpPost("register")]//api/account/register
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
-{
-    if (await EmailExists(registerDto.Email)) return BadRequest("Email Taken");
-
-    using var hmac = new HMACSHA512();
-
-    var user = new AppUser
     {
-        DisplayName = registerDto.DisplayName,
-        Email = registerDto.Email,
-        PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-        PasswordSalt = hmac.Key
-    };
+        if (await EmailExists(registerDto.Email)) return BadRequest("Email Taken");
 
-    context.Users.Add(user);
-    await context.SaveChangesAsync();
+        using var hmac = new HMACSHA512();
 
-    return user.ToDto(tokenService);
-}
-[HttpPost("login")]
-public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        var user = new AppUser
+        {
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user.ToDto(tokenService);
+        
+    }
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
         var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email);
         if (user == null) return Unauthorized("Invalid Email address");
